1864168d4af756ed220170415034d09588eee397
Adrien Guffens <adrien.guffens@epitech.eu>
2013-05-09 23:00:17 +0800

    db pre integration OptionsActivity

--- gen/android/games/minesweeper/R.java
+++ gen/android/games/minesweeper/R.java
@@ -49,15 +49,29 @@ public final class R {
         public static final int tile=0x7f020009;
     }
     public static final class id {
-        public static final int HighScoresTitle=0x7f0a0002;
+        public static final int HighScoresTitle=0x7f0a0010;
         public static final int Timer=0x7f0a0000;
-        public static final int action_settings=0x7f0a0008;
-        public static final int imageView1=0x7f0a0005;
-        public static final int ll=0x7f0a0004;
+        public static final int action_settings=0x7f0a0016;
+        public static final int b_remove=0x7f0a000f;
+        public static final int e_name=0x7f0a000d;
+        public static final int imageView1=0x7f0a0013;
+        public static final int ll=0x7f0a0012;
+        public static final int lvl_easy=0x7f0a0004;
+        public static final int lvl_hard=0x7f0a0006;
+        public static final int lvl_medium=0x7f0a0005;
         public static final int main_list_view=0x7f0a0001;
-        public static final int score_list_view=0x7f0a0003;
-        public static final int textView1=0x7f0a0006;
-        public static final int textView2=0x7f0a0007;
+        public static final int rg_lvl=0x7f0a0003;
+        public static final int rg_size=0x7f0a0008;
+        public static final int score_list_view=0x7f0a0011;
+        public static final int size_big=0x7f0a000b;
+        public static final int size_medium=0x7f0a000a;
+        public static final int size_small=0x7f0a0009;
+        public static final int t_level=0x7f0a0002;
+        public static final int t_name=0x7f0a000c;
+        public static final int t_remove=0x7f0a000e;
+        public static final int t_size=0x7f0a0007;
+        public static final int textView1=0x7f0a0014;
+        public static final int textView2=0x7f0a0015;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -73,14 +87,21 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f070000;
+        public static final int b_remove=0x7f070011;
+        public static final int big_label=0x7f070010;
         public static final int difficulty_title=0x7f070005;
         public static final int easy_label=0x7f070006;
         public static final int hard_label=0x7f070008;
         public static final int help=0x7f070004;
+        public static final int level_label=0x7f07000d;
         public static final int medium_label=0x7f070007;
+        public static final int name_label=0x7f07000b;
         public static final int options=0x7f070003;
         public static final int play=0x7f070001;
+        public static final int remove_label=0x7f07000e;
         public static final int scores_records=0x7f070002;
+        public static final int size_label=0x7f07000c;
+        public static final int small_label=0x7f07000f;
         public static final int smiley=0x7f07000a;
         public static final int timer=0x7f070009;
     }
--- src/android/games/minesweeper/OptionsActivity.java
+++ src/android/games/minesweeper/OptionsActivity.java
@@ -4,6 +4,7 @@ package android.games.minesweeper;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.text.StaticLayout;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -18,32 +19,47 @@ import android.widget.RadioGroup.OnCheckedChangeListener;
 public class OptionsActivity extends Activity {
 
 
+	String TAG = "OptionsActivity";
+	
 	RadioGroup	rg_lvl;
 	RadioButton rb_selected_lvl;
 	RadioGroup	rg_size;
 	RadioButton rb_selected_size;
-	EditText	set_player_name;
-	Button		remove;
-	String		player_name;
+	EditText	txt_player_name;
+	Button		bt_remove;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_options);
+	Option optionRecord;
 
-		rg_lvl = (RadioGroup) super.findViewById(R.id.rg_lvl);
-		rg_size = (RadioGroup) super.findViewById(R.id.rg_size);
-		set_player_name = (EditText) super.findViewById(R.id.e_name);
-		remove = (Button) super.findViewById(R.id.b_remove);
-		
+	private void setupView() {
+		rg_lvl = (RadioGroup)super.findViewById(R.id.rg_lvl);
+		rg_size = (RadioGroup)super.findViewById(R.id.rg_size);
+		txt_player_name = (EditText)super.findViewById(R.id.e_name);
+		bt_remove = (Button)super.findViewById(R.id.b_remove);
+	}
+
+	private void setupListner() {
 		rg_lvl.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			public void onCheckedChanged(RadioGroup group, int checkedId) {
 				int selected_lvl = rg_lvl.getCheckedRadioButtonId();
-				rb_selected_lvl = (RadioButton) findViewById(selected_lvl);
-				//Toast.makeText(getBaseContext(), (String) rb_selected_lvl.getText(), Toast.LENGTH_SHORT).show();
-				Log.d("Checked SELECTED IS : ", (String) rb_selected_lvl.getText());
 
+				rb_selected_lvl = (RadioButton) findViewById(selected_lvl);
+				Log.d("Checked SELECTED IS : ", (String) rb_selected_lvl.getText() + " | " + selected_lvl);
+
+				switch (selected_lvl) {
+				case R.id.lvl_easy:
+					optionRecord.setLevel(0);
+					break;
+				case R.id.lvl_medium:
+					optionRecord.setLevel(1);
+					break;
+				case R.id.lvl_hard:
+					optionRecord.setLevel(2);
+					break;
+
+				default:
+					break;
+				}
 			}
 		});
 
@@ -51,39 +67,141 @@ public class OptionsActivity extends Activity {
 
 			public void onCheckedChanged(RadioGroup group, int checkedId) {
 				int selected_size = rg_size.getCheckedRadioButtonId();
+
 				rb_selected_size = (RadioButton) findViewById(selected_size);
-				//Toast.makeText(getBaseContext(), (String) rb_selected_size.getText(), Toast.LENGTH_SHORT).show();
 				Log.d("Checked SELECTED IS : ", (String) rb_selected_size.getText());
 
+				switch (selected_size) {
+				case R.id.size_small:
+					optionRecord.setSize(0);
+					break;
+				case R.id.size_medium:
+					optionRecord.setSize(1);
+					break;
+				case R.id.size_big:
+					optionRecord.setSize(2);
+					break;
+
+				default:
+					break;
+				}
 			}
 		});
 
-		set_player_name.setOnKeyListener(new OnKeyListener() {
-			
+		txt_player_name.setOnKeyListener(new OnKeyListener() {
+
 			@Override
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				 if((event.getAction()==KeyEvent.ACTION_DOWN)&&
-		                    (keyCode==KeyEvent.KEYCODE_ENTER)){
-		                player_name = set_player_name.getText().toString();
-		                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-		                imm.hideSoftInputFromWindow(set_player_name.getWindowToken(),0);
-		                Log.d("Edit TExt PLAYER NAME : ", player_name);
-		                return true;
-		            }
+				if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+						(keyCode == KeyEvent.KEYCODE_ENTER)) {
+					String player_name = txt_player_name.getText().toString();
+					InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(txt_player_name.getWindowToken(), 0);
+
+					//INFO: setting name
+					optionRecord.setName(player_name);
+
+					Log.d("Edit Text PLAYER NAME : ", player_name);
+					return true;
+				}
 				return false;
 			}
 		});
-		
-		remove.setOnClickListener(new OnClickListener() {
-			
+
+		bt_remove.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View arg0) {
 				// TODO remove all record from the DATA BASE
 				Log.d("Button Remove : ", "ok");
 			}
 		});
-		
 	}
 
+	private int buttonIdForLevel(final int level) {
+		int buttonId = 0;
 
+		switch (level) {
+		case 0:
+			buttonId = R.id.lvl_easy;	
+			break;
+		case 1:
+			buttonId = R.id.lvl_medium;
+			break;
+		case 2:
+			buttonId = R.id.lvl_hard;
+			break;
+
+		default:
+			break;
+		}
+		return buttonId;
+	}
+
+	private int buttonIdForSize(final int size) {
+		int buttonId = 0;
+
+		switch (size) {
+		case 0:
+			buttonId = R.id.size_small;
+			break;
+		case 1:
+			buttonId = R.id.size_medium;
+			break;
+		case 2:
+			buttonId = R.id.size_big;
+			break;
+
+		default:
+			break;
+		}
+		return buttonId;
+	}
+
+	private void setupOption() {
+		//INFO: debug
+		optionRecord = new Option();
+		optionRecord.setId(0);
+		optionRecord.setLevel(1);
+		optionRecord.setName("Adril");
+		optionRecord.setSize(1);
+
+		int lvlButtonId = buttonIdForLevel(1);//(optionRecord.getLevel());
+		rg_lvl.check(lvlButtonId);
+		int sizeButtonId = buttonIdForSize(1);//(optionRecord.getSize());
+		rg_size.check(sizeButtonId);
+		txt_player_name.setText(optionRecord.getName());
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_options);
+
+		//TODO: set Option with model
+		// Options options = Global.getOption(); 
+		//setControl with options
+		//edition -> modification de Options
+		//Quit view -> save option
+
+		setupView();
+		setupListner();		
+		setupOption();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "on DESTROY");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		
+		Log.d(TAG, "on STOP");
+		//TODO: save db
+
+	}
 }
