26db8299b0dcea64c06d7ae01f5a96060956b3bd
Kenni Bertucat <kenni.bertucat@gmail.com>
2013-05-16 23:21:24 +0800

    Mise a jour Score Records et Mise a jour Activity Help

--- src/android/games/minesweeper/ScoreRecordsActivity.java
+++ src/android/games/minesweeper/ScoreRecordsActivity.java
@@ -19,7 +19,7 @@ import android.widget.AdapterView.OnItemClickListener;
  
 public class ScoreRecordsActivity extends BaseActivity implements OnClickListener {
  
-        class ScoreRecordsText { public String text1, text2; }
+        class ScoreRecordsText { public String text1, text2; int img; }
  
         private String TAG = "ScoreActivity";
  
@@ -41,8 +41,8 @@ public class ScoreRecordsActivity extends BaseActivity implements OnClickListene
                 final ListView listView = (ListView)findViewById(R.id.score_list_view);
                 text = new ArrayList<ScoreRecordsText>(getScoreRecordsText(Scores));
                 ArrayList<ListItemScoreRecords> listItemArray = new ArrayList<ListItemScoreRecords>();
-        listItemArray.addAll(getScoreRecordsList(Scores));
-        listView.setAdapter(new ScoreRecordsListAdapter(listItemArray, getApplicationContext()));
+                listItemArray.addAll(getScoreRecordsList(Scores));
+                listView.setAdapter(new ScoreRecordsListAdapter(listItemArray, getApplicationContext()));
                 listView.setOnItemClickListener(new OnItemClickListener() {
  
                         @Override
@@ -78,12 +78,7 @@ public class ScoreRecordsActivity extends BaseActivity implements OnClickListene
                         ListItemScoreRecords item_details = new ListItemScoreRecords();
                         Log.d(TAG, "GetScoreRecordList text1: " + scoreRecordsText.text1 + " text2:  " + scoreRecordsText.text2);
                         item_details.setText(scoreRecordsText.text1, scoreRecordsText.text2);
-                        if (score < 400)
-                                item_details.setImage(image[2]);
-                        else if (score >= 400 && score <= 2000)
-                                item_details.setImage(image[1]);
-                        else if (score > 3000)
-                                item_details.setImage(image[0]);
+                        item_details.setImage(image[scoreRecordsText.img]);
                         results.add(item_details);
                 }
                 return results;
@@ -92,7 +87,7 @@ public class ScoreRecordsActivity extends BaseActivity implements OnClickListene
         private ArrayList<ScoreRecordsText> getScoreRecordsText(List<Score> score_list) {
                 ArrayList<ScoreRecordsText> results = new ArrayList<ScoreRecordsText>();
                 String level = "NULL";
-                for (int i = 0; i < score_list.size(); i++) {
+                for (int i = score_list.size()-1; i >= 0 ; i--) {
                         Score score;
                         ScoreRecordsText score_records_text = new ScoreRecordsText();
                         score = score_list.get(i);
@@ -107,6 +102,12 @@ public class ScoreRecordsActivity extends BaseActivity implements OnClickListene
                                 level = "Hard";
                                 break;
                         }
+                        if (score.getScore() < 40)
+                        	score_records_text.img = 2;
+                        else if (score.getScore() >= 40 && score.getScore() <= 70)
+                        	score_records_text.img = 1;
+                        else if (score.getScore() > 70)
+                        	score_records_text.img = 0;
                         score_records_text.text1 = "Level : " + level + " -- Score: " + score.getScore() + " in " + Globals.gameSizeToString(score.getSize()) + " size";
                         score_records_text.text2 = score.getName() + " has finished the game";
                         score_records_text.text2 +=  " in " + Scores.get(i).getDuration() + " seconds.";
